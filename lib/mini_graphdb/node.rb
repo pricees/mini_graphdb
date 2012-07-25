be57a29852de8281c42cb683731d37e5967e201f
Edward Price <ted.price@gmail.com>
2012-07-25 18:13:18 -0500

    Typo

--- lib/mini_graphdb/node.rb
+++ lib/mini_graphdb/node.rb
@@ -2,12 +2,12 @@ module MiniGraphdb
   class Node < OpenStruct
 
     def inbound_edges
-      @inbound_edges ||= Edges.new
+      @inbound_edges ||= EdgeSet.new
     end
     alias_method :inbound, :inbound_edges
 
     def outbound_edges
-      @outbound_edges ||= Edges.new
+      @outbound_edges ||= EdgeSet.new
     end
     alias_method :outbound, :outbound_edges
     alias_method :edges,    :outbound_edges
